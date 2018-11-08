d0464fcee2fb1d13b9de38d6f2986604188bb880
a lan <bluenowte97@gmail.com>
2018-11-08 21:31:20 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,9 +5,8 @@ def second_challenge
    meat: ["chicken", "steak", "salmon"],
    grains: ["rice", "pasta"]
   }
-
-  #code your solution here!
-
+nested_array = groceries.values
+nested_array.flatten
   
 
 end
